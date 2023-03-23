d846af77e40b0a98b10548be33aae1b2d41c2186
jose cabreja <cabreja@yahoo.com>
2023-03-23 07:49:00 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(ralf) {
+    cats.push(ralf);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  
+  function prependCat(jay) {
+    return [jay, ...cats];
+  }
+
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
